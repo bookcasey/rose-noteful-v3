2ea0acc8cc096befb6093b210ed82970c55dd59a
Rose Sorfa <rose.otter.2015@gmail.com>
2018-08-14 15:42:55 -0500

    finished all endpoints

--- routes/notes.js
+++ routes/notes.js
@@ -4,47 +4,117 @@ const express = require('express');
 
 const router = express.Router();
 
+// const mongoose = require('mongoose');
+
+const Note = require('../models/note');
+
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  const searchTerm = req.query.searchTerm; 
+  let filter = {};
+  const filterArray = [];
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
-
+  if (searchTerm) {
+    const title = { 'title': {$regex: searchTerm, $options: 'i' }};
+    const content = { 'content': {$regex: searchTerm, $options: 'i' }};
+    filterArray.push(title);
+    filterArray.push(content);
+    filter.$or = filterArray; 
+  }
+  
+  Note
+    .find(filter)
+    .sort({ updatedAt: 'desc' })
+    .then(notes => {
+      res.json(notes);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
+ 
+// console.log('Get All Notes');
+// res.json([
+//   { id: 1, title: 'Temp 1' },
+//   { id: 2, title: 'Temp 2' },
+//   { id: 3, title: 'Temp 3' }
+// ]);
+
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
-
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+  const id = req.params.id;
+  Note
+    .findById(id)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+  if (!('title' in req.body)) {
+    const message = 'Missing title in request body';
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  const newItem = {
+    title: req.body.title,
+    content: req.body.content
+  };
+    
+  Note
+    .create(newItem)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+    
+  // console.log('Create a Note');
+  // res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
 
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+    
+  const updateItem = {};
 
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
+  Object.keys(req.body).forEach(key => updateItem[key] = req.body[key]);
+
+  Note
+    .findByIdAndUpdate(id, updateItem, {new: true})
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+
+  // console.log('Update a Note');
+  // res.json({ id: 1, title: 'Updated Temp 1' });
 
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
-
-  console.log('Delete a Note');
-  res.status(204).end();
+  const idRemove = '5b73299d6bf68dc852d73799';
+  Note
+    .findByIdAndRemove(idRemove)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
